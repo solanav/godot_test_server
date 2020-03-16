39ace1df31f7d249460e72531ab80775435b6e73
solanav <solanav@qq.com>
2020-03-16 20:42:12 +0100

    Rotation with quaternions is now working

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ class Player():
         self.pos = None
         self.id = md5(str(addr).encode()).hexdigest()
 
-    def update_pos(self, posx, posy, posz):
-        pos = (posx, posy, posz)
-        if self.pos != pos:
-            self.pos = pos
+    def update_pos(self, posx, posy, posz, rotx, roty, rotz, rotw):
+        pos = (posx, posy, posz, rotx, roty, rotz, rotw)
+        self.pos = pos
 
-    def send_update(self, sock, id, posx, posy, posz):
+    def send_update(self, sock, id, posx, posy, posz, rotx, roty, rotz, rotw):
         print('{} {} > {}, {}, {}'.format(id, self.addr, posx, posy, posz))
-        sock.sendto("{},{},{},{}".format(id, posx, posy, posz).encode(), self.addr)
+        sock.sendto("{},{},{},{},{},{},{},{}".format(id, posx, posy, posz, rotx, roty, rotz, rotw).encode(), self.addr)
 
 def main():
     ''' Main loop '''
@@ -32,14 +31,25 @@ def main():
     while True:
         data, addr = sock.recvfrom(MAX_PACKET)
 
-        # Deserialize coordinates
+        # Clean packet
         pos = data.decode('ascii').replace('(', '').replace(')', '').replace(' ', '')
         parsed = pos.split(',')
-        if len(parsed) != 3:
+        print(parsed)
+        if len(parsed) != 7:
             continue
+
+        # Deserialize position
         posx = float(parsed[0])
         posy = float(parsed[1])
         posz = float(parsed[2])
+
+        # Deserialize rotation
+        rotx = float(parsed[3])
+        roty = float(parsed[4])
+        rotz = float(parsed[5])
+        rotw = float(parsed[6])
+
+        # Deserialize hash
         id = md5(str(addr).encode()).hexdigest()
 
         # Looking through player list to update pos
@@ -47,18 +57,19 @@ def main():
         for player in player_list:
             if player.addr == addr:
                 # Update player
-                player.update_pos(posx, posy, posz)
+                player.update_pos(posx, posy, posz, rotx, roty, rotz, rotw)
+                player.send_update(sock, id, posx + 2, posy + 5, posz, rotx, roty, rotz, rotw)
                 found = True
             else:
                 # Send update to other players
-                if player.pos != (posx, posy, posz):
-                    player.send_update(sock, id, posx, posy, posz)
+                if player.pos != (posx, posy, posz, rotx, roty, rotz, rotw):
+                    player.send_update(sock, id, posx, posy, posz, rotx, roty, rotz, rotw)
 
         # New player connected
         if not found:
             print('New player connected on {}'.format(addr))
             p = Player(addr)
-            p.update_pos(posx, posy, posz)
+            p.update_pos(posx, posy, posz, rotx, roty, rotz, rotw)
             player_list.append(p)
 
 if __name__ == '__main__':
